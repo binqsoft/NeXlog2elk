b64f77008d93280d103646fe1f5e3696e9beaa86
liang.shao@bird01.com <liangeeks@gmail.com>
2017-08-09 17:20:58 +0800

    加入登录页面

--- up.sh
+++ up.sh
@@ -1,2 +1,1 @@
-sudo chmod -R 777 elasticsearch/esdata/
-docker-compose -f docker-compose.yml -f extensions/logspout/logspout-compose.yml up
+docker-compose -f docker-compose.yml -f extensions/logspout/logspout-compose.yml up -d
